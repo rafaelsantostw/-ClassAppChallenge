5b4a86eb6ba3358a718f6aef04a2d706f31c83b9
Rafael Ramos <rafaelsantostw@gmail.com>
2020-03-31 13:59:29 -0300

    Update code

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ fs.createReadStream('input.csv')
   .on('end', () => {
 
     const header = response[0]
-    //console.log(header[4])
 
     function validateEmail(email) {
       var resp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -28,13 +27,13 @@ fs.createReadStream('input.csv')
       return resp.test(phone.replace(" ",""));
     }
 
-    response.forEach((element) => {
+    response.forEach((coluna) => {
 
       let adresses = []
 
       //Coluna Email Responsavel, Pai
-      if(element[4].length !== 0){
-        var resulElement = element[4].replace("/"," ").split(' ')
+      if(coluna[4].length !== 0){
+        var resulElement = coluna[4].replace("/"," ").split(' ')
         adresses.push({
           type: header[4].split(' ')[0],
           tags:[
@@ -46,39 +45,39 @@ fs.createReadStream('input.csv')
       }
 
       //Coluna Phone Pai
-      if(element[5].length > 10){
+      if(coluna[5].length > 10){
         adresses.push({
           type: header[5].split(' ')[0],
           tags:[
             header[5].split(' ')[1]
           ],
-          adress: element[5].replace(/^/,'55').replace(/["'()]/g,"").replace(" ","")
+          adress: coluna[5].replace(/^/,'55').replace(/["'()]/g,"").replace(" ","")
         })
       }
 
       //Coluna Phone Responsavel, Mae
-      if(element[6].length !== 0){
+      if(coluna[6].length !== 0){
         adresses.push({
           type: header[6].split(' ')[0],
           tags:[
             header[6].split(' ')[1].replace(",",""),
             header[6].split(' ')[2]
           ],
-          adress: element[6].replace(/^/,'55').replace(/["'()]/g,"").replace(" ","")
+          adress: coluna[6].replace(/^/,'55').replace(/["'()]/g,"").replace(" ","")
         })
       }
       
       //Coluna Email Mae
-      if(element[7].length !== 0 && validateEmail(element[7]) == true){
+      if(coluna[7].length !== 0 && validateEmail(coluna[7]) == true){
         //validando se os emails dao iguais para unir as tags
-        if(element[7] === element[8]){
+        if(coluna[7] === coluna[8]){
           adresses.push({
             type: header[7].split(' ')[0],
             tags:[
               header[7].split(' ')[1],
               header[8].split(' ')[1]
             ],
-            adress: element[7]
+            adress: coluna[7]
           })
         }else{
           adresses.push({
@@ -86,41 +85,41 @@ fs.createReadStream('input.csv')
             tags:[
               header[7].split(' ')[1]
             ],
-            adress: element[7]
+            adress: coluna[7]
           })
         }
       }
 
       //Coluna Email Aluno
-      if(element[8].length !== 0 && element[8] !== element[7]){
+      if(coluna[8].length !== 0 && coluna[8] !== coluna[7]){
         adresses.push({
           type: header[8].split(' ')[0],
           tags:[
             header[8].split(' ')[1]
           ],
-          adress: element[8]
+          adress: coluna[8]
         })
       }
 
       //Coluna Phone Aluno
-      if(element[9].length !== 0 && validatePhone(element[9]) == true){
+      if(coluna[9].length !== 0 && validatePhone(coluna[9]) == true){
         adresses.push({
           type: header[9].split(' ')[0],
           tags:[
             header[9].split(' ')[1]
           ],
-          adress: element[9].replace(/^/,'55').replace(/["'()]/g,"").replace(" ","")
+          adress: coluna[9].replace(/^/,'55').replace(/["'()]/g,"").replace(" ","")
         })
       }
 
       //Incluindo primeira coluna 'class' no array classes
-      let classes = element[2].split('/').map(ele => ele.trim())
+      let classes = coluna[2].split('/').map(ele => ele.trim())
 
       //Incluindo segunda coluna 'class' no array classes
-      element[3].length !== 0 ? classes.push(element[3]) : null
+      coluna[3].length !== 0 ? classes.push(coluna[3]) : null
     
       //Filtro para pegar o registro que se repete
-      let r = register.filter(val => val.eid === element[1])
+      let r = register.filter(val => val.eid === coluna[1])
 
       if(r.length > 0){
 
@@ -135,25 +134,25 @@ fs.createReadStream('input.csv')
         })
         
         //Incluindo adresses do registro repetido no principal
-        register[1].adresses.push(...adresses)
+        r[0].adresses.push(...adresses)
 
-        //incluindo primera e segunda coluna do segundo registro John Due 1
-        register[1].classes.push(element[2])
-        register[1].classes.push(...element[3].split(',').map(ele => ele.trim()))
+        //incluindo primera e segunda coluna do segundo registro
+        r[0].classes.push(coluna[2])
+        r[0].classes.push(...coluna[3].split(',').map(ele => ele.trim()))
 
         //validar campo see_all
-        seeVal = element[11] == 'yes' ? true : false
-        register[1].see_all !==  seeVal ? register[1].see_all = true : null
+        seeVal = coluna[11] == 'yes' ? true : false
+        r[0].see_all !==  seeVal ? r[0].see_all = true : null
 
       }else{
         //Incluir novo registro
         register.push({
-          fullname: element[0],
-          eid: element[1],
-          classes: classes,
+          fullname: coluna[0],
+          eid: coluna[1],
+          classes: classes.length == 1 ? classes[0]:classes,
           adresses: adresses,
-          invisible: element[10] == 1 ? true : false,
-          see_all: element[11] == 'yes' ? true : false
+          invisible: coluna[10] == 1 ? true : false,
+          see_all: coluna[11] == 'yes' ? true : false
         })
         
       }
